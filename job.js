2caf15535a96a883c529f72464b62fef12b55241
vupadhayayx86 <vupadhayayx86@gmail.com>
2024-05-12 19:53:59 +0530

    Added lambda files and build specs
    Changes to run the build
    
    Changes to build specs for node 18
    
    Revert "Changes to build specs for node 18"
    
    This reverts commit 3d8e9c9a9cc94c8d6ba1f87f443e3aa0ab9f377a.
    
    Removing x from runtime version
    
    Chaning the node version to 18
    
    Added package.json
    
    Changes in buildspec file to store in zip after deploytment
    
    Changes to check fileupdate

---
+++ job.js
@@ -0,0 +1,6 @@
+export const handler=(event)=>{
+    console.log("Entered Lambda Function....")
+    console.log(event)
+    console.log("This is after deployment....")
+    console.log("Exiting Lambda Function....After Changes")
+}

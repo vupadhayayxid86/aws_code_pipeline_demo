4f387b7c5ab6f6751a21d97cd1df3c9fc18b5e5a
vupadhayayx86 <vupadhayayx86@gmail.com>
2024-05-12 20:25:17 +0530

    Changes to run the build

--- job.js
+++ job.js
@@ -1,5 +1,5 @@
 export const handler=(event)=>{
     console.log("Entered Lambda Function....")
     console.log(event)
-    console.log("Exiting Lambda Function....")
+    console.log("Exiting Lambda Function....After Changes")
 }

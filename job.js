7cebedb8ffdba85c6bb2e5ad003f39ff5e66a7cc
vupadhayayx86 <vupadhayayx86@gmail.com>
2024-06-02 16:26:31 +0530

    Changes to increase commits
    Added lambda files and build specs
    
    Changes to run the build
    
    Changes to build specs for node 18
    
    Revert "Changes to build specs for node 18"
    
    This reverts commit 3d8e9c9a9cc94c8d6ba1f87f443e3aa0ab9f377a.
    
    Removing x from runtime version
    
    Chaning the node version to 18
    
    Added package.json
    
    Changes in buildspec file to store in zip after deploytment

--- job.js
+++ job.js
@@ -1,6 +1,11 @@
 export const handler=(event)=>{
     console.log("Entered Lambda Function....")
     console.log(event)
+<<<<<<< HEAD
     console.log("This is after deployment....")
     console.log("Exiting Lambda Function....After Changes")
+    console.log("Changes to make new commit..")
+=======
+    console.log("Exiting Lambda Function....After Changes")
+>>>>>>> 4f387b7 (Changes to run the build)
 }
